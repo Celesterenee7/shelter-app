69254dafae4439b20897a71a8f11d014835b7e2e
Celeste Morton <creneemorton@gmail.com>
2020-01-31 10:13:51 -0800

    add additional columns to animals table

--- db/migrate/20200131181007_create_animals.rb
+++ db/migrate/20200131181007_create_animals.rb
@@ -2,7 +2,11 @@ class CreateAnimals < ActiveRecord::Migration[5.2]
   def change
     create_table :animals do |t|
       t.column(:name, :string)
-      t.column(:animal_type, :string)
+      t.column(:creature, :string)
+      t.column(:gender, :string)
+      t.column(:born, :date)
+      t.column(:profile, :string)
+      t.column(:fav_food, :string)
 
       t.timestamps()
     end
