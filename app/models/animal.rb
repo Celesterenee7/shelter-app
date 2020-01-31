4633cf59f55b05f243e27da2a5fb6ed65b2f5cf1
Celeste Morton <creneemorton@gmail.com>
2020-01-31 11:18:06 -0800

    add validations and scope for animal to models

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,11 @@
 class Animal < ApplicationRecord
+
+  validates :name, presence: true
+  validates :creature, presence: true
+  validates :gender, presence: true
+  validates :born, presence: true
+  validates :profile, presence: true
+  validates :fav_food, presence: true
+
+  scope :search, -> (name_parameter) { where("name like ?", "%#{name_parameter}%") }
 end
---
+++ spec/requests/delete_animals_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+
+# describe "post a animal route", :type => :request do
+
+#   before do
+#     post '/animals', params: { :name => 'test_name', :creature => 'test_creature', :gender => 'test_gender' }
+
+#     delete '/animals/.id', params: { :name => 'test_name', :creature => 'test_creature', :gender => 'test_gender' }
+#   end
+# end
