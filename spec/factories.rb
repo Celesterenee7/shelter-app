0e04d5f9535f0b7dba77e51571ca99a159a91a1e
Celeste Morton <creneemorton@gmail.com>
2020-01-31 10:53:03 -0800

    add factorybot for testing the api

---
+++ spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+    factory(:animal) do
+      name {'Meika'}
+      creature {'Pomeranian'}
+      gender {'Female'}
+      born {'2010-6-13'}
+      profile {'Meikas is the cutest dog in the world!'}
+      fav_food {'Chicken'}
+      # priority {1}
+    end
+  end
