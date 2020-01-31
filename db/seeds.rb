c44dc88c94facd8df4d4d13d09a95a631a610029
Celeste Morton <creneemorton@gmail.com>
2020-01-31 09:09:07 -0800

    update verison of ruby and bundle again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 class Seed
 
-    def self.begin
-      seed = Seed.new
-      seed.generate_animals
-    end
-  
-    def generate_animals
-      20.times do |i|
-        animal = animal.create!(
-          name: Faker::GreekPhilosophers.name,
-          animal_type: Faker::Creature::Animal.name
-        )
-        puts "animal #{i}: Animal name is #{animal.name} and animal type is #{animal.animal_type}."
-      end
+  def self.begin
+    seed = Seed.new
+    seed.generate_animals
+  end
+
+  def generate_animals
+    20.times do |i|
+      animal = Animal.create!(
+        name: Faker::GreekPhilosophers.name,
+        animal_type: Faker::Creature::Animal.name
+      )
+      puts "Animals #{i}: name is #{animal.name} and animal type is #{animal.animal_type}."
     end
   end
-  
-  Seed.begin
+end
+
+Seed.begin
